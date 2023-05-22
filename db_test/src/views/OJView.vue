ddcac32657ed2fcaabe61a7c689ad6f6575298c5
Ztrura <809772167@qq.com>
2023-05-22 08:57:21 +0800

    update

--- db_test/src/views/OJView.vue
+++ db_test/src/views/OJView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="oj">
     <h1>This is oj page</h1>
+    <OnlineJudge msg="1111"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import OnlineJudge from '@/components/OnlineJudge.vue'
+
+export default {
+  name: 'OJView',
+  components: {
+    OnlineJudge
+}
+}
+</script>
